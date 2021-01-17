b82b3e69bc539a1e86797added72240edf9a28d4
Ricardo Perez martinez <ricardoperezmartinez@MacBook-Pro-de-Ricardo.local>
2021-01-18 00:15:16 +0100

    Datos enviados a la API de Mailchimp

--- app.js
+++ app.js
@@ -3,23 +3,53 @@ const app = express();
 const bodyParser = require("body-parser");
 const https = require("https");
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
 app.use(express.static("public"));
 
-app.get("/", function(req, res){
-  res.sendFile(__dirname+"/signup.html")
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html")
 })
 
-app.post ("/", function(req,res){
+app.post("/", function(req, res) {
 
   const email = (req.body.email);
   const nombre = (req.body.name);
   const apellido = (req.body.apellido);
 
-  
+  const data = {
+    members:[
+      {
+        email_address:email,
+        status: "subscribed",
+        merge_fields : {
+          FNAME:nombre,
+          LNAME:apellido,
+        }
 
+      }
+    ]
+  }
+  const jsonData = JSON.stringify(data);
 
+  const url = 'https://us7.api.mailchimp.com/3.0/lists/247e04ae7b'
+  const options = {
+
+    method: 'POST',
+    auth: "pepe:2afc401f8076e234cd8cc122ef9c2629-us7"
+  }
+
+  const request = https.request(url, options, function(res) {
+    console.log(res.statusCode);
+    res.on("data", function (data){
+      console.log(JSON.parse(data));
+    })
+  })
+
+  request.write(jsonData)
+  request.end();
 
 
 
@@ -35,6 +65,6 @@ app.post ("/", function(req,res){
 
 
 
-app.listen (3000, function(){
+app.listen(3000, function() {
   console.log("Server running on port 3000");
 })

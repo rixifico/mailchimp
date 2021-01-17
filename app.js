0ce36e684f90ee4088aa65e9941651bfc18b6f4f
Ricardo Perez martinez <ricardoperezmartinez@MacBook-Pro-de-Ricardo.local>
2021-01-17 23:21:58 +0100

    obtencion de datos

--- app.js
+++ app.js
@@ -1,8 +1,31 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const https = require("https");
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname+"/signup.html")
+})
+
+app.post ("/", function(req,res){
+
+  const email = (req.body.email);
+  const nombre = (req.body.name);
+  const apellido = (req.body.apellido);
+
+  
 
 
 
+
+
+
+
+})
 
 
 
